36536ca4a6d51dc1105c7ee49e7be93a7a885e80
tolu <olabiyitolu@gmail.com>
2024-02-13 11:02:16 +0100

    update

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -7,30 +7,40 @@ class FileStorage:
     __objects = {}
 
     def all(self):
+        """
+        Returns the dictionary of all objects.
+        """
         return FileStorage.__objects
 
     def new(self, obj):
+        """
+        Adds a new object to the storage.
+        """
         object_class_name = obj.__class__.__name__
         key = "{}.{}".format(object_class_name, obj.id)
         FileStorage.__objects[key] = obj
 
     def save(self):
+        """
+        Saves the objects to the JSON file.
+        """
         all_objects = FileStorage.__objects
-        object_dict = {}
-        for obj in all_objects.keys():
-            object_dict[obj] = all_objects[obj].to_dict()
-        with open(FileStorage.__file_path, "w", encoding = "utf-8") as file:
+        object_dict = {key: obj.to_dict() for key, obj in all_objects.items()}
+        with open(FileStorage.__file_path, "w", encoding="utf-8") as file:
             json.dump(object_dict, file)
 
     def reload(self):
+        """
+        Reloads objects from the JSON file.
+        """
         if os.path.isfile(FileStorage.__file_path):
-            with open(FileStorage.__file_path, "r", encoding = "utf-8") as file:
+            with open(FileStorage.__file_path, "r", encoding="utf-8") as file:
                 try:
                     object_dict = json.load(file)
-                    for key, value in object_dict.items():
+                    for key, values in object_dict.items():
                         class_name, object_id = key.split(".")
-                        class_nm = eval(class_name)
-                        instance = class_nm(**values)
+                        class_obj = eval(class_name)
+                        instance = class_obj(**values)
                         FileStorage.__objects[key] = instance
-                except Exception:
-                    pass
+                except Exception as e:
+                    print(f"Error reloading objects: {e}")
